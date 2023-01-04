46d8a4224020c1227b9e9d13bfc8a164b9ed70b0
Oluwatoyin Ariyo <35049514+toyinariyo@users.noreply.github.com>
2023-01-04 17:12:15 +0000

    Create tip-calculator main.cs
    With comment about it printing out the wrong answer (14 instead of 15.4). Cannot figure out why

---
+++ Beginner/tip-calculator/main.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Here is Toyin's tip calculator!");
+    Console.WriteLine("What was the total bill?");
+    decimal bill = decimal.Parse(Console.ReadLine());
+    Console.WriteLine("Was the tip 10, 12 or 15?");
+    int tip = int.Parse(Console.ReadLine());
+    Console.WriteLine("How many people are splitting the bill?");
+    int people_split = int.Parse(Console.ReadLine());
+    int tip_percentage = tip / 100;
+    decimal total_tip = bill * tip_percentage;
+    decimal total_bill = bill + total_tip;
+    decimal bill_per_person = total_bill / people_split;
+    decimal final_amount = Math.Round(bill_per_person, 2);
+    Console.WriteLine("Each person should pay: " + "$" + final_amount); //Prints out wrong answer: 14 instead of 15.4 
+    }
+}
